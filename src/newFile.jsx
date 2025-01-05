226329af8bdb06cb6c2181417dc4b2c1d644c8ea
saad ahsan <saadahsankhan890@gmail.com>
2025-01-05 19:47:25 +0500

    add a text

--- src/newFile.jsx
+++ src/newFile.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const newFile = () => {
     return (
-        <div>newFile</div>
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga tempore nisi alias rerum, accusamus ex ad, suscipit magnam, consectetur praesentium quasi? At deserunt optio totam nam reiciendis dolorem voluptas quibusdam.
+            </p>
+            <div>newFile</div>
+        </>
     )
 }
